867bcfc3a13920dfe93f8fd73a661f2d7f44d142
zouweilin <zouweilin@9realms.co>
2022-02-25 19:26:02 +0800

    remvove init bat

--- h.bat
+++
@@ -1,7 +0,0 @@
-echo "# ExtractSignatureFromPicture" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/luoyexk/ExtractSignatureFromPicture.git
-git push -u origin main
